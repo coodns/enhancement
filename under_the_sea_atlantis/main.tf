be316289c20783a874c0318f8fab58fb0a3ad6c0
codnstj <mail@chaewoon.me>
2025-01-06 08:36:28 +0900

    add creation of terraform

---
+++ under_the_sea_atlantis/main.tf
@@ -0,0 +1,48 @@
+module "atlantis" {
+  source  = "terraform-aws-modules/atlantis/aws"
+
+  name = "atlantis"
+
+  # ECS Container Definition
+  atlantis = {
+    environment = [
+      {
+        name  = "ATLANTIS_GH_USER"
+        value = "<Gitlab Atlantis User>"
+      },
+      {
+        name  = "ATLANTIS_REPO_ALLOWLIST"
+        value = "<gitlab allowlist>"
+      },
+    ]
+    secrets = [
+      {
+        name      = "ATLANTIS_GITLAB_TOKEN"
+        valueFrom = "<Gitlab Atlantis Access Token>"
+      },
+      {
+        name      = "ATLANTIS_GITLAB_WEBHOOK_SECRET"
+        valueFrom = "<Secret Token For Webhook Secret>"
+      },
+    ]
+  }
+
+  # ECS Service
+  service = {
+    task_exec_secret_arns = [
+      "this is the aws secret manager arn",
+      "it contains contianer definition's secret",
+    ]
+    # Provide Atlantis permission necessary to create/destroy resources
+    tasks_iam_role_policies = {
+      AdministratorAccess = "arn:aws:iam::aws:policy/AdministratorAccess"
+    }
+  }
+  service_subnets = "insert where you want to place to ecs services"
+  vpc_id          = "insert where you want to place to ecs services"
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
