5992de96fd1bcaae356ce05cd3497bc2302967f7
codnstj <mail@chaewoon.me>
2025-01-06 09:11:51 +0900

    add config to set a region

--- cross_like_toni_kroos/cross_like_toni_kroos/lambda_handler.py
+++ cross_like_toni_kroos/cross_like_toni_kroos/lambda_handler.py
@@ -2,16 +2,20 @@ import os
 import json
 import boto3
 import get_crossaccount
+from botocore.config import Config
 
 
 def lambda_handler(event, context):
     json_region = os.environ['AWS_REGION']
+    cross_config = Config(
+        region_name = json_region,
+    )
     cred = get_crossaccount.get_cro.assume_role("account_id", "role_name")
     b3client = boto3.session(
         aws_access_key_id=cred.aws_access_key,
         aws_secret_access_key=cred.aws_secret_access_key,
-        aws_session_token=cred.aws_session_token
-
+        aws_session_token=cred.aws_session_token,
+        config=cross_config
     )
     return {
         "statusCode": 200,
@@ -19,6 +23,6 @@ def lambda_handler(event, context):
             "Content-Type": "application/json"
         },
         "body": json.dumps({
-            "Region ": json_region
+            "AccountID": b3client.client('sts').get_caller_identity().get('Account')
         })
     }
