975ac4e2e069dbd5ce48d0e468f8930513650df5
codnstj <mail@chaewoon.me>
2025-01-14 16:55:35 +0900

    integrate module in a one files

--- cross_like_toni_kroos/cross_like_toni_kroos/lambda_handler.py
+++ cross_like_toni_kroos/cross_like_toni_kroos/lambda_handler.py
@@ -1,7 +1,8 @@
 import json
 import boto3
 import os
-import get_crossaccount
+import random
+
 from botocore.config import Config
 import mysql.connector
 
@@ -21,7 +22,7 @@ def lambda_handler(event, context):
         region_name=json_region
     )
 
-    cred = get_crossaccount.get_cro.assume_role(target_account_id, target_role_name)
+    cred = get_cro.assume_role(target_account_id, target_role_name)
 
     ssm_client = boto3.client(
         'ssm',
@@ -32,11 +33,13 @@ def lambda_handler(event, context):
     )
 
     try:
-        db_host = ssm_client.get_parameter(Name=ssm_host_param, WithDecryption=True)['Parameter']['Value']
-        db_port = int(ssm_client.get_parameter(Name=ssm_port_param, WithDecryption=True)['Parameter']['Value'])
-        db_name = ssm_client.get_parameter(Name=ssm_name_param, WithDecryption=True)['Parameter']['Value']
-        db_user = ssm_client.get_parameter(Name=ssm_user_param, WithDecryption=True)['Parameter']['Value']
-        db_password = ssm_client.get_parameter(Name=ssm_password_param, WithDecryption=True)['Parameter']['Value']
+        db_host = str(get_parameter(ssm_client, ssm_host_param))
+        print(f"Database host: {db_host}")
+
+        db_port = int(ssm_port_param)
+        db_name = ssm_name_param
+        db_user = get_parameter(ssm_client, ssm_user_param)
+        db_password = get_parameter(ssm_client, ssm_password_param)
 
         conn = mysql.connector.connect(
             host=db_host,
@@ -50,7 +53,7 @@ def lambda_handler(event, context):
         )
 
         cursor = conn.cursor()
-        cursor.execute("SELECT 1")
+        cursor.execute("SELECT * FROM ")
         cursor.fetchone()
         cursor.close()
 
@@ -87,3 +90,52 @@ def lambda_handler(event, context):
     finally:
         if 'conn' in locals() and conn.is_connected():
             conn.close()
+
+
+def get_parameter(client, name):
+    try:
+        response = client.get_parameter(Name=name, WithDecryption=True)
+        return response['Parameter']['Value']
+    except client.exceptions.ParameterNotFound:
+        raise ValueError(f"SSM Parameter '{name}' not found.")
+    except Exception as e:
+        raise RuntimeError(f"Failed to fetch parameter '{name}': {str(e)}")
+
+
+class get_cro:
+    @staticmethod
+    def assume_role(account_id, role_name, *, session_name=None, transient_role_credentials=None):
+        """
+        Assume role in an account and return credentials
+
+        Args:
+            account_id (str): ID of the account to assume role in
+            role_name (str): Name of the role to assume
+            session_name (str): optional name for the assume_role session
+            transient role (dict): result of a different assume_role call with credentials of the transient role
+
+        Returns:
+            dict: Credentials for the assumed role
+        """
+        # set up boto3 client to assume role adding transient role credentials if provided
+        if not transient_role_credentials:
+            sts_client = boto3.client('sts')
+        else:
+            sts_client = boto3.client('sts', **transient_role_credentials)
+
+        # generate random session name if not provided
+        if not session_name:
+            session_name = f'AssumeRoleSession{random.randint(1, 1000)}'
+
+        response = sts_client.assume_role(
+            RoleArn=f'arn:aws:iam::{account_id}:role/{role_name}',
+            RoleSessionName=session_name,
+        )
+
+        # creds = {
+        #     'aws_access_key_id': response['Credentials']['AccessKeyId'],
+        #     'aws_secret_access_key': response['Credentials']['SecretAccessKey'],
+        #     'aws_session_token': response['Credentials']['SessionToken']
+        # }
+
+        return response
