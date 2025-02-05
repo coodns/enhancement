4791946cb00910d603ba9a27851cfba86d8a23b5
coodnstj <mail@chaewoon.me>
2025-02-05 10:13:31 +0900

    add provider set

---
+++ it_is_seaman_not_seman/terraform/vpc/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  backend "remote" {
+    organization = "enhancement"
+
+  workspaces {
+    name = "vpc"
+  }
+}
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-2"
+}
